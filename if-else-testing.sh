8893a71450f0b47d9cd4b26150d8d46cc7c53f95
Rich ZW <rich@rwhifonline.net>
2024-02-09 15:05:34 -0500

    added comment information

--- if-else-testing.sh
+++ if-else-testing.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# this script was written to test conditional 
+# functionality based on the shell environment
+# variables, specifically whether or not the 
+# current shell is running in a terminator
+# terminal emulator
+
+# the actual script is in the GitHub repo
+# see https://github.com/WriteRead024/BashScripts
+# file bash-pm2-start.sh for an example usage
+
 SETOUTPUT=$(set | grep "terminator")
 
 if [[ $SETOUTPUT != "" ]]
